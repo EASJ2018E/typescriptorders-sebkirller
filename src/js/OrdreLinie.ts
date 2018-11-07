889e76879121391b31663d2503bc3c43c12ba0a1
Sebastian Petersen <sebastian@srmail.dk>
2018-11-07 09:43:48 +0100

    Added method, BeregnSum.

--- src/js/OrdreLinie.ts
+++ src/js/OrdreLinie.ts
@@ -5,10 +5,15 @@ export class OrdreLinie implements IOrdreLinie {
     public ProductId: number;
     public UnitPrice: number;
     public UnitPriceDiscount: number;
+
     constructor(ordreQty: number, productId: number, unitPrice: number, unitPriceDiscount: number) {
         this.OrderQty = ordreQty;
         this.ProductId = productId;
         this.UnitPrice = unitPrice;
         this.UnitPriceDiscount = unitPriceDiscount;
+    }
+
+    public BeregnSum() {
+        return this.OrderQty * (this.UnitPrice - this.UnitPriceDiscount);
     }
 }
